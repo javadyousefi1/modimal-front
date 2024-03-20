0001b36a9f5f527c1f366528b60fe4c4c377e7a9
Javad Yousefi <javadyousefi9000@gmail.com>
2024-03-20 12:37:36 +0330

    add slider to app

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,14 @@ import CartBox from "../components/cart/CartBox";
 // elements
 import Button from "../components/elements/Button";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+// import required modules
+import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css/pagination";
+
 const HomePage = () => {
   return (
     <>
@@ -32,12 +40,54 @@ const HomePage = () => {
         <section className="px-5 mt-5">
           <h1 className="text-xl font-bold">Best Sellers</h1>
           <div className="mt-2">
-            <CartBox
-              title="tailored stretch"
-              desc="turn it up pants"
-              price={180}
-              colors={["#748C70", "#7DC3EB", "#0C0C0C"]}
-            />
+            <Swiper
+              pagination={{ clickable: true }}
+              modules={[Pagination, Autoplay]}
+              spaceBetween={68}
+              slidesPerView={2}
+              onSlideChange={() => console.log("slide change")}
+              // onSwiper={(swiper) => console.log(swiper)}
+              style={{
+                "--swiper-pagination-color": "var(--color-primary)", //custome style for pagination bullet
+              }}
+            >
+              <SwiperSlide>
+                {" "}
+                <CartBox
+                  title="tailored stretch"
+                  desc="turn it up pants"
+                  price={180}
+                  colors={["#7DC3EB", "#0C0C0C", "#748C70"]}
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                {" "}
+                <CartBox
+                  title="tailored stretch"
+                  desc="turn it up pants"
+                  price={180}
+                  colors={["#7DC3EB", "#0C0C0C", "#748C70"]}
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                {" "}
+                <CartBox
+                  title="tailored stretch"
+                  desc="turn it up pants"
+                  price={180}
+                  colors={["#7DC3EB", "#0C0C0C", "#748C70"]}
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                {" "}
+                <CartBox
+                  title="tailored stretch"
+                  desc="turn it up pants"
+                  price={180}
+                  colors={["#7DC3EB", "#0C0C0C", "#748C70"]}
+                />
+              </SwiperSlide>
+            </Swiper>
           </div>
         </section>
       </main>
