c3293111e19a7829b9dce8c76e797a5051b60982
Javad Yousefi <javadyousefi9000@gmail.com>
2024-03-26 19:09:00 +0330

    convert route file to ts

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,9 +1,16 @@
 // page
+import { ReactNode } from "react";
 import HomePage from "../pages/HomePage";
 import NotFound from "../pages/NotFound";
 
+interface RouteType {
+  id: number;
+  path: string;
+  title: string;
+  component: ReactNode
+}
 // array of routes
-export const routes = [
+export const routes: RouteType[] = [
   { id: 0, path: "*", title: "404", component: NotFound },
   { id: 1, path: "/", title: "main page", component: HomePage },
 ];
