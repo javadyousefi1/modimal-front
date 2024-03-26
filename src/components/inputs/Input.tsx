ff2e9bca48c79f23ec7aa00bfb9a991e5f1d6eff
Javad Yousefi <javadyousefi9000@gmail.com>
2024-03-26 23:34:35 +0330

    fix register style

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -11,7 +11,7 @@ interface InputPropType {
 }
 const Input = ({ type, onChange, name, placeHolder, className, value, error }: InputPropType) => {
 
-    const inputStyle = `w-full px-3 py-2.5 text-xs border outline-none border-neutral-700 placeholder-neutral-700 ${className} ${error ? "border-red-400" : "border-neutral-700"}`
+    const inputStyle = `w-full px-3 py-2.5 text-xs border outline-none border-neutral-700 placeholder-neutral-700 transition linear duration-150 ${className} ${error ? "border-red-400 bg-red-50" : "border-neutral-700"}`
 
     return (<>
         <input placeholder={placeHolder} value={value} type={type} onChange={onChange} name={name} className={inputStyle} />
