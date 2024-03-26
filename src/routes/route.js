fb43b9f9ec6125ac83e6465b8ff0167857f5a937
Javad Yousefi <javadyousefi9000@gmail.com>
2024-03-26 20:10:33 +0330

    fix title of index & delete extra file

--- src/routes/route.js
+++
@@ -1,9 +0,0 @@
-// page
-import HomePage from "../pages/HomePage";
-import NotFound from "../pages/NotFound";
-
-// array of routes
-export const routes = [
-  { id: 0, path: "*", title: "404", component: NotFound },
-  { id: 1, path: "/", title: "main page", component: HomePage },
-];
