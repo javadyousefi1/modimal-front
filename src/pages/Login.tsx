f78c9de1ae681b668b3274fa2f921b615ecde10b
Javad Yousefi <javadyousefi9000@gmail.com>
2024-03-27 12:19:09 +0330

    fix center

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,7 +9,7 @@ const Login = () => {
         <div className="flex flex-col items-center justify-center md:flex-row gap-x-32">
             {/* Register page image */}
             <img src={LoginImage} alt="login" className="h-[218px] w-full md:h-[80vh] md:w-[400px]  object-cover" />
-            <div className="w-full">
+            <div className="w-full md:w-max">
                 {/* title */}
                 <h1 className="mt-6 font-bold text-center text-black md:text-3xl">Log In</h1>
                 <div className="px-5 mt-8 max-w-[392px]">
