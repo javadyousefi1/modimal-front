7114eeb4ee533fd5704691be5df93b8d95c0a233
Javad Yousefi <javadyousefi9000@gmail.com>
2024-04-26 16:43:51 +0330

    fix login url

--- src/api/login.ts
+++ src/api/login.ts
@@ -2,6 +2,6 @@ import { LoginFormType } from "@types";
 import axios from "axios";
 
 export async function loginUser(userData: LoginFormType): Promise<any> {
-    return axios.post(`https://modimal-shop.runflare.run/api/login`, userData)
+    return axios.post(`https://modimal-shop.runflare.run/login`, userData)
 }
 
