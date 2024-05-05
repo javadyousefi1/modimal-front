17fcc2bdb1c41d4e71ecf3cbcc6ff8f830c559ac
Javad Yousefi <javadyousefi9000@gmail.com>
2024-05-05 23:15:29 +0330

    fix url

--- src/api/register.ts
+++ src/api/register.ts
@@ -1,7 +1,7 @@
 import { RegisterFormType } from "@types";
-import axios from "axios";
+import axios from "./axios";
 
 export async function registerUser(userData: RegisterFormType): Promise<any> {
-    return axios.post(`https://modimal-shop.runflare.run/register`, userData)
+    return axios.post(`/register`, userData)
 }
 
