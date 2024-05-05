e8975ec6686ec4becd1c65f6e3f0c3942ea124f0
Amirhossein Hemmati <amirhemmati15@yahoo.com>
2024-05-05 22:13:12 +0430

    fix images of product

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -5,12 +5,7 @@ import ProductFilter from "@components/products/ProductFilter"
 // SVG
 import MobileBanner from "../assets/images/productswiper3.svg";
 import DesktopBanner from "../assets/images/productswiper4.svg";
-import product1 from "../assets/images/product1.svg";
-import product2 from "../assets/images/product2.svg";
-import product3 from "../assets/images/product3.svg";
-import product4 from "../assets/images/product4.svg";
-import product5 from "../assets/images/product5.svg";
-import product6 from "../assets/images/product6.svg";
+import product1 from "../assets/images/productImg(1).png";
 const Products: React.FC = () => {
   let faqBreadcrumbs = [
     { id: 1, text: "Home", href: "/" },
@@ -32,7 +27,7 @@ const Products: React.FC = () => {
     },
     {
       id: 2,
-      image: product2,
+      image: product1,
       title: "Essential T-shirt",
       off: "20%",
       newproduct: true,
@@ -43,7 +38,7 @@ const Products: React.FC = () => {
     },
     {
       id: 3,
-      image: product3,
+      image: product1,
       title: "Shirt Dress",
       off: null,
       newproduct: true,
@@ -54,7 +49,7 @@ const Products: React.FC = () => {
     },
     {
       id: 4,
-      image: product4,
+      image: product1,
       title: "Rule zip Jacket",
       off: null,
       newproduct: false,
@@ -65,7 +60,7 @@ const Products: React.FC = () => {
     },
     {
       id: 5,
-      image: product5,
+      image: product1,
       title: "New Age Linen",
       off: "30%",
       newproduct: false,
@@ -76,7 +71,7 @@ const Products: React.FC = () => {
     },
     {
       id: 6,
-      image: product6,
+      image: product1,
       title: "Boss Pullover",
       off: null,
       newproduct: false,
