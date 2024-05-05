deb981544b61998da93ca7bd91018e8c8b5d8408
Amirhossein Hemmati <amirhemmati15@yahoo.com>
2024-05-05 21:59:20 +0430

    add container to footer

--- src/components/products/ProductFilterContent.tsx
+++ src/components/products/ProductFilterContent.tsx
@@ -1,7 +1,18 @@
+import Input from "@components/inputs/Input"
+import { Controller, useForm } from "react-hook-form"
 
-const ProductFilterContent: React.FC = () => {
+const ProductFilterContent: React.FC = ({content}) => {
+
+  const {handleSubmit, control} = useForm({})
   return (
-    <div>ProductFilterContent</div>
+    <form>
+      <Controller name="sortBy" control={control} render={({ field: { onChange, value } }) => (
+        <>
+        <label htmlFor="sortBy1"></label>
+          <Input onChange={onchange} value={value} type="checkbox"/>
+        </>
+        )}/>
+    </form>
   )
 }
 
