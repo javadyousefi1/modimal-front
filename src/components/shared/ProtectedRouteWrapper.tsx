93eefe682dc89073e5746c048f76c25eb57c762e
Javad Yousefi <javadyousefi9000@gmail.com>
2024-05-10 15:12:15 +0330

    add protect route

---
+++ src/components/shared/ProtectedRouteWrapper.tsx
@@ -0,0 +1,35 @@
+import { useEffect, type ReactNode } from "react";
+// redux
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
+// antd
+import { Spin } from "antd";
+import { useNavigate } from "react-router-dom";
+
+const ProtectedRouteWrapper: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const navigate = useNavigate();
+
+  const { loggedIn, loading } = useSelector(
+    (state: RootState) => state.usersSlice
+  );
+
+  useEffect(() => {
+    if (!loading && !loggedIn) {
+      navigate("/");
+    }
+  }, [loggedIn]);
+
+  if (loading) {
+    return (
+      <div className="container flex flex-col items-start min-h-screen px-6 mt-4 max-w-7xl md:px-0">
+        <Spin spinning={loading} fullscreen />
+      </div>
+    );
+  }
+
+  return children;
+};
+
+export default ProtectedRouteWrapper;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,46 +1,40 @@
 import { useEffect } from "react";
 // redux
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 // store
 import { RootState } from "../store/store";
 // rrd
 import { useNavigate } from "react-router-dom";
 // antd
-import {  Spin,  } from "antd";
+import { Spin } from "antd";
 // components
 import ProfileSidebar from "@/components/user/profile/ProfileSidebar";
+import ProtectedRouteWrapper from "@/components/shared/ProtectedRouteWrapper";
 
 const Profile = () => {
-  const { loggedIn, loading, userData } = useSelector(
-    (state: RootState) => state.usersSlice
-  );
+  // const { loggedIn, loading, userData } = useSelector(
+  //   (state: RootState) => state.usersSlice
+  // );
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
+  // useEffect(() => {
+  //   if (!loading && !userData) {
+  //     navigate("/");
+  //   }
+  // }, [userData, loading, loggedIn]);
 
-  useEffect(() => {
-    if (!loading && !userData) {
-      navigate("/");
-    }
-  }, [userData, loading, loggedIn]);
-
-  if (!loading && !loggedIn) {
-    navigate("/");
-    return;
-  }
-
-  if (loading) {
-    return (
-      <div className="container flex flex-col items-start min-h-screen px-6 mt-4 max-w-7xl md:px-0">
-        <Spin spinning={loading} fullscreen />
-      </div>
-    );
-  }
+  // if (!loading && !loggedIn) {
+  //   navigate("/");
+  //   return;
+  // }
 
   return (
-    <div className="container flex flex-col items-start min-h-screen px-6 mt-4 max-w-7xl md:px-0">
-      <ProfileSidebar />
-    </div>
+    <ProtectedRouteWrapper>
+      <div className="container flex flex-col items-start min-h-screen px-6 mt-4 max-w-7xl md:px-0">
+        <ProfileSidebar />
+      </div>
+    </ProtectedRouteWrapper>
   );
 };
 
