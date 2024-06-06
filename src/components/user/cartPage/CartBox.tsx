fb8397002500e204a2ec66e9136a5e0d539a3dbe
Javad Yousefi <javadyousefi9000@gmail.com>
2024-06-06 18:21:31 +0330

    done cart action

--- src/components/user/cartPage/CartBox.tsx
+++ src/components/user/cartPage/CartBox.tsx
@@ -1,3 +1,4 @@
+import { formatPrice } from "@/utils/helper";
 import image from "@assets/images/productImg(1).png";
 
 const CartBox: React.FC<{
@@ -12,13 +13,28 @@ const CartBox: React.FC<{
     <div className="flex justify-between h-[120px] gap-x-5 mt-6">
       {/* prodcut image */}
       <div>
-        <img src={image} className="h-[120px]" />
+        <img src={bannerUrl} className="h-[120px] object-fill w-[80px]" />
       </div>
       {/* product detail */}
       <div className="flex flex-col justify-between flex-1">
         {/* header */}
-        <div>
+        <div className="flex items-center justify-between">
           <h1 className="font-semibold">{name}</h1>
+          {/* cross icon */}
+          <button type="button">
+            <svg
+              width="22"
+              height="22"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
+                fill="#0C0C0C"
+              />
+            </svg>
+          </button>
         </div>
         {/* size */}
         <div>
@@ -32,7 +48,7 @@ const CartBox: React.FC<{
         </div>
         {/* price */}
         <div className="flex justify-between">
-          <span className="text-sm font-bold">{price} $</span>
+          <span className="text-sm font-bold">{formatPrice(price)} $</span>
           {/* cart operation buttons */}
           <div className="flex items-center justify-center bg-primary-50 gap-x-2">
             <button className="text-lg font-bold">
--- src/pages/user/Cart.tsx
+++ src/pages/user/Cart.tsx
@@ -1,4 +1,5 @@
 import { getAllCartByUser } from "@/api/cart";
+import ProtectedRouteWrapper from "@/components/shared/ProtectedRouteWrapper";
 import CartBox from "@/components/user/cartPage/CartBox";
 import useFetch from "@/hooks/useFetch";
 import { Alert, Spin } from "antd";
@@ -9,38 +10,38 @@ const Cart = () => {
 
   const cartData = data?.data?.data;
 
-    // show loading until server response
-    if (loading) {
-      return (
-        <div className="flex flex-col items-center justify-center mt-5 gap-y-4">
-          <Spin />
-          <p>Getting Cart Info ...</p>
-        </div>
-      );
-    }
+  // show loading until server response
 
   return (
-    <section className="px-4">
-      {/* title */}
-      <h1 className="mt-5 text-xl font-bold text-center">Your cart</h1>
-      {/* show message if cart is empty */}
-      {cartData?.length === 0 && (
-        <Alert message="Your Cart Is Empty" className="mt-5" showIcon />
-      )}
+    <ProtectedRouteWrapper>
+      <section className="px-4">
+        {/* title */}
+        <h1 className="mt-5 text-xl font-bold text-center">Your cart</h1>
+        {loading && (
+          <div className="flex flex-col items-center justify-center mt-5 gap-y-4">
+            <Spin />
+            <p>Getting Cart Info ...</p>
+          </div>
+        )}
+        {/* show message if cart is empty */}
+        {cartData?.length === 0 && (
+          <Alert message="Your Cart Is Empty" className="mt-5" showIcon />
+        )}
 
-      {/* show cart */}
+        {/* show cart */}
 
-      {cartData?.map((item) => (
-        <CartBox
-          color={item.color}
-          name={item.productName}
-          price={item.price}
-          bannerUrl={item.bannerUrl}
-          count={item.count}
-          size={item.size}
-        />
-      ))}
-    </section>
+        {cartData?.map((item) => (
+          <CartBox
+            color={item.color}
+            name={item.productName}
+            price={item.price}
+            bannerUrl={item.bannerUrl}
+            count={item.count}
+            size={item.size}
+          />
+        ))}
+      </section>
+    </ProtectedRouteWrapper>
   );
 };
 
